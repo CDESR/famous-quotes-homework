0db26f055d72ebc62a4b95dbb3323f55ac43e87f
Sebastian Sim <phistando@yahoo.com>
2016-08-14 10:10:37 +0800

    added simple weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,70 @@ $(function(){
   }
 });
 });
+
+
+
+$(function(){
+  $weatherButton = $('#sgweather-btn');
+  $forecastContent = $('#weather-forecast');
+  $loader = $('.loader');
+
+  api2_url = "https://simple-weather.p.mashape.com/weather?lat=1.3521&lng=103.8198";
+
+  $weatherButton.on('click', function(){
+    $.ajax({
+      method: "GET",
+      url: api2_url,
+      headers: {'X-Mashape-Key': 'fKNj4SkY1lmshrYv3plHC6jXCNmdp1YPo3XjsnujdpbI1rHohn',
+           'Accept': 'text/plain'},
+      dataType: 'text',
+
+      beforeSend: function() {
+        $loader.show();
+      }
+
+
+    }).success(successFunction)
+      .fail(failFunction);
+
+    function successFunction(data){
+      $loader.hide();
+      $forecastContent.text(data);
+    }
+
+    function failFunction(jqXHR, textStatus, errorThrown){
+      console.log(errorThrown);
+    }
+
+  });
+});
+
+
+// $(function(){
+//   $gerWeatherButton = $('#gerweather-btn');
+//   $forecastContent = $('#weather-forecast');
+//   api2_url = "https://simple-weather.p.mashape.com/weather?lat=52.5200&lng=13.4050";
+//
+//   $gerWeatherButton.on('click', function(){
+//     $.ajax({
+//       method: "GET",
+//       url: api2_url,
+//       headers: {'X-Mashape-Key': 'fKNj4SkY1lmshrYv3plHC6jXCNmdp1YPo3XjsnujdpbI1rHohn',
+//            'Accept': 'text/plain'},
+//       dataType: 'text',
+//
+//
+//     }).success(successFunction)
+//       .fail(failFunction);
+//
+//     function successFunction(data){
+//       alert(data);
+//       $forecastContent.text(data);
+//     }
+//
+//     function failFunction(jqXHR, textStatus, errorThrown){
+//       console.log(errorThrown);
+//     }
+//
+//   });
+// });
